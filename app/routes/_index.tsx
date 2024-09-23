b83fa2981573375eb6593377e07c347e0eb8fd6b
Sung Choi <sung@sungthecoder.com>
2024-09-23 16:19:44 -0700

    Install tailwindcss for styling framework

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -10,7 +10,7 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Troop 466</h1>
+      <h1 className="text-3xl font-bole underline">Welcome to Troop 466</h1>
       <ul>
         <li>
           <a
