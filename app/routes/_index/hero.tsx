5a738821a16065561d3e28fc513d74fa6cb586a0
Sung Choi <sung@sungthecoder.com>
2024-10-28 21:27:01 -0700

    Update hero color to white

--- app/routes/_index/hero.tsx
+++ app/routes/_index/hero.tsx
@@ -7,8 +7,8 @@ export const Hero = ({ title, lead }: HeroProps) => (
     <div className="hero-overlay bg-opacity-60 bg-gradient-to-r from-troop466-900/60"></div>
     <div className="hero-content text-neutral-content text-center">
       <div className="max-w-lg">
-        <h1 className="mb-5 text-5xl font-bold">{title}</h1>
-        <p className="mb-5">{lead}</p>
+        <h1 className="mb-5 text-5xl font-bold text-troop466-50">{title}</h1>
+        <p className="mb-5 text-troop466-50">{lead}</p>
         {/* <button className="btn btn-primary">Get Started</button> */}
       </div>
     </div>
