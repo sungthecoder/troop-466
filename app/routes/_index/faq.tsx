99de12dbb958236ffa46d78aee247b81b7f64ce5
meuhl :D <105988747+mp1678@users.noreply.github.com>
2025-01-30 09:17:42 -0800

    update faq file to use a map

--- app/routes/_index/faq.tsx
+++ app/routes/_index/faq.tsx
@@ -1,30 +1,23 @@
-export const FAQ = () => {
+import { FAQItem } from "~/lib/fetch-faqs";
+
+type FAQs = {
+    faqs: FAQItem[];
+};
+
+export const FAQ = ({ faqs }: FAQs) => {
     return (
-        <section className="py-12 bg-troop466-200">
-        <div className="h-60">FAQ
-          <div className="collapse collapse-plus bg-base-200">
-            <input type="radio" name="my-accordion-3" defaultChecked/>
-            <div className="collapse-title text-xl font-medium">How can I/my scout join the troop?</div>
-            <div className="collapse-content">
-              <p>Insert Google Drive Link Here w Info</p>
+        <section className="py-12 bg-us-200"> 
+            <div className="h-60">FAQ
+                {faqs.map(({ question, answer }, index) => (
+                    <div key={index} className="collapse collapse-plus bg-base-200">
+                        <input type="radio" name="my-accordion-3" defaultChecked/>
+                        <div className="collapse-title text-xl font-medium">{question}</div>
+                        <div className="collapse-content">
+                            <p>{answer}</p>
+                        </div>
+                    </div>
+                ))}
             </div>
-          </div>
-          <div className="collapse collapse-plus bg-base-200">
-            <input type="radio" name="my-accordion-3" />
-            <div className="collapse-title text-xl font-medium">How can I access scoutbook?</div>
-            <div className="collapse-content">
-              <p>Insert Scoutbook Link Here</p>
-            </div>
-          </div>
-          <div className="collapse collapse-plus bg-base-200">
-            <input type="radio" name="my-accordion-3" />
-            <div className="collapse-title text-xl font-medium">An additional section, just in case.</div>
-            <div className="collapse-content">
-              <p>If another thing comes in mind, put it here!</p>
-            </div>
-          </div>
-        </div>
-      </section>
+        </section>
     );
 };
-  
